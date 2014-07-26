d354a7b1941ca0fdbdd1449a42c1232f2ec1fe8a
mareldic <macio218@wp.pl>
2014-07-26 18:36:17 +0200

    Changes in application module. Lerning process.

--- zf_nauka/module/Application/config/module.config.php
+++ zf_nauka/module/Application/config/module.config.php
@@ -8,6 +8,10 @@
  */
 
 return array(
+    'application' => array(
+	   "name" => "Training Center",
+       "version" => "1.0",
+    ),
     'router' => array(
         'routes' => array(
             'home' => array(
--- zf_nauka/module/Application/src/Application/Controller/IndexController.php
+++ zf_nauka/module/Application/src/Application/Controller/IndexController.php
@@ -16,6 +16,26 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        return new ViewModel();
+        //return new ViewModel();
+//         return array(
+//         	"version" => "0.0.1",
+//             "applicationName" => "Training Center",
+//         );
+
+        $serviceLocator = $this->getServiceLocator();
+        $config = $serviceLocator->get('config');
+        return array(
+        	"version" => $config['application']["version"],
+            "applicationName" => $config["application"]["name"]
+        );
+        
+    }
+    
+    public function aboutAction()
+    {
+//         $view = new ViewModel();
+//         $view->setTemplate("/application/index/about");
+//         return $view;
+        return array();
     }
 }
