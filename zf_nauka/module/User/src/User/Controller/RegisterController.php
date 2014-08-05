337932aeff56fad9f0000a1dae363c1ebfc6c661
mareldic <macio218@wp.pl>
2014-08-05 19:36:51 +0200

    controller

--- zf_nauka/module/User/src/User/Controller/RegisterController.php
+++ zf_nauka/module/User/src/User/Controller/RegisterController.php
@@ -26,6 +26,7 @@ class RegisterController extends AbstractActionController
         $view = new ViewModel(array("form"=>$form));
         return $view;
     }
+    
     public function processAction()
     {
         if (!$this->request->isPost())
@@ -53,10 +54,37 @@ class RegisterController extends AbstractActionController
             return $model;
         }
         
+        // Create user
+        $this->createUser($form->getData());
+        
         return $this->redirect()->toRoute(NULL, array(
         	"controller" => "register",
             "action" => "confirm"
-        ));
-        
+        ));        
+    }
+    
+    public function confirmAction()
+    {
+    	$viewModel  = new ViewModel();
+    	return $viewModel;
+    }
+    
+    protected function createUser(array $data)
+    {
+    
+    	$sm = $this->getServiceLocator();
+    	$dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+    
+    	$resultSetPrototype = new \Zend\Db\ResultSet\ResultSet();
+    	$resultSetPrototype->setArrayObjectPrototype(new \User\Model\User);
+    	$tableGateway       = new \Zend\Db\TableGateway\TableGateway('user' /* table name  */, $dbAdapter, null, $resultSetPrototype);
+    
+    	$user = new User();
+    	$user->exchangeArray($data);
+    
+    	$userTable = new UserTable($tableGateway);
+    	$userTable->saveUser($user);
+    
+    	return true;
     }
 }
